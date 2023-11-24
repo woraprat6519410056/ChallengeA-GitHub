4f818dc54e432e533ead6171135e8ddb5914e8cf
woraprat6519410056 <s6519410056@sau.ac.th>
2023-11-24 11:41:48 +0700

    0000

--- workshop01.py
+++ workshop01.py
@@ -1,25 +1,26 @@
 def count_words(sentence):
     words = sentence.split()
  
-   
     word_count = {}
- 
+    duplicate_words = []
+   
     for word in words:
-       
         if word in word_count:
             word_count[word] += 1
+            if word not in duplicate_words:
+                duplicate_words.append(word)  
         else:
             word_count[word] = 1
  
-   
     print(f"มีคำทั้งหมด {len(words)} คำ")
-    lem = print(f"มีคำที่ปรากฏมากกว่า 1 ครั้ง: ")
-    for word, count in word_count.items():
-        if count > 1:
-            print(f"คำว่า '{word}' ปรากฏ {count} ครั้ง")
+    for word in duplicate_words:
+        print(f"คำว่า '{word}' ปรากฏ {word_count[word]} ครั้ง")
+ 
+    return len(duplicate_words), duplicate_words
  
 print("++++++++++++++++++++++++++++++++++++++")
 user_sentence = input("ป้อนข้อความ: ")
 print("++++++++++++++++++++++++++++++++++++++")
-count_words(user_sentence)
+duplicate_count, duplicate_words = count_words(user_sentence)
+print(f"มีคำซ้ำกันทั้งหมด {duplicate_count} คำ คำคือ {', '.join(duplicate_words)}")
 print("++++++++++++++++++++++++++++++++++++++")
