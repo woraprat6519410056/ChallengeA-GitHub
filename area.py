caf3ecc4a31896c30cb9f290a970838c2aa3e2db
woraprat6519410056 <s6519410056@sau.ac.th>
2023-11-24 11:35:56 +0700

    2512

--- area.py
+++ area.py
@@ -1,6 +1,12 @@
 import math
  
-def rectangle_volume(width, length, height):
+def rectangle_area(width, length):
+    return width * length
+ 
+def circle_area(radius):
+    return math.pi * radius**2
+ 
+def prism_volume(width, length, height):
     return width * length * height
  
 def sphere_volume(radius):
--- cube.py
+++ cube.py
@@ -1,12 +1,6 @@
 import math
  
-def rectangle_area(width, length):
-    return width * length
- 
-def circle_area(radius):
-    return math.pi * radius**2
- 
-def prism_volume(width, length, height):
+def rectangle_volume(width, length, height):
     return width * length * height
  
 def sphere_volume(radius):
